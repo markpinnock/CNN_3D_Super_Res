5f93ae3c58fc452bc0cad429a4c5432b3dbc9beb
Mark <rmappin@ucl.ac.uk>
2019-08-17 11:00:49 +0100

    Added imgLoader2

--- utils/functions.py
+++ utils/functions.py
@@ -9,6 +9,7 @@ def diceIndexCalc(vol_A, vol_B):
     return numer / denom
 
 
+""" Loads images in minbatches """
 def imgLoader(hi_list, lo_list, indices):
     TENSOR_DIMS = 5
 
@@ -49,3 +50,47 @@ def imgLoader(hi_list, lo_list, indices):
     return hi_img, lo_img
 
 
+""" Loads images in minbatches """
+def imgLoader2(hi_list, lo_list, indices):
+    TENSOR_DIMS = 5
+
+    try:
+        hi_mb = [hi_list[i] for i in indices]
+        lo_mb = [lo_list[i] for i in indices]
+    except:
+        print(indices, file=sys.stderr)
+        raise IndexError("Index out of range")
+
+    if len(hi_mb) != len(lo_mb):
+        print(hi_mb, lo_mb, file=sys.stderr)
+        raise ValueError("hi_mb and lo_mb lengths do not match")
+
+    if [vol[:-5] for vol in hi_mb] != [vol[:-5] for vol in lo_mb]:
+        print(hi_mb, lo_mb, file=sys.stderr)
+        raise ValueError("hi_mb and lo_mb names do not match")
+
+    if 'L' in [vol[-26:-4] for vol in hi_mb]:
+        print(hi_mb, file=sys.stderr)
+        raise ValueError("Lo vol in hi_mb")
+
+    if 'H' in [vol[-26:-4] for vol in lo_mb]:
+        print(lo_mb, file=sys.stderr)
+        raise ValueError("Hi vol in lo_mb")  
+
+    hi_img = np.expand_dims(np.stack([np.float32(np.load('Hi/' + img)) for img in hi_mb], axis=0), axis=4)
+    pre_lo_img = np.expand_dims(np.stack([np.float32(np.load('Lo/' + img)) for img in lo_mb], axis=0), axis=4)
+    lo_img = pre_lo_img[:, :, :, 0::4, :]
+
+    if hi_img.shape[3:5] != (12, 1):
+        print(hi_img.shape,  file=sys.stderr)
+        raise ValueError("hi_mb tensor shape incorrect")
+    
+    if lo_img.shape[3:5] != (3, 1):
+        print(lo_img.shape,  file=sys.stderr)
+        raise ValueError("lo_mb tensor shape incorrect")
+
+    if len(hi_img.shape) != TENSOR_DIMS:
+        print(hi_img.shape, lo_img.shape, file=sys.stderr)
+        raise ValueError("Tensor dimensions not correct")
+
+    return hi_img, lo_img
