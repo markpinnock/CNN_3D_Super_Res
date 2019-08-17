214623675ecf9f257afaf88791d3c4a191dec189
Mark <rmappin@ucl.ac.uk>
2019-08-17 18:03:52 +0100

    Changed mean loss and reg to sum over batches

--- utils/losses.py
+++ utils/losses.py
@@ -11,11 +11,11 @@ def calcDice(vol_A, vol_B):
 
 
 def lossL1(hi_img, pred_img):
-    return tf.reduce_mean(tf.abs(hi_img - pred_img), axis=[0, 1, 2, 3])
+    return tf.reduce_sum(tf.reduce_mean(tf.abs(hi_img - pred_img), axis=[1, 2, 3, 4]), axis=None)
 
 
 def lossL2(hi_img, pred_img):
-    return tf.reduce_mean(tf.square(hi_img - pred_img), axis=[0, 1, 2, 3])
+    return tf.reduce_sum(tf.reduce_mean(tf.square(hi_img - pred_img), axis=[1, 2, 3, 4]), axis=None)
 
 
 def calcPSNR(hi_img, pred_img):
@@ -52,7 +52,7 @@ def regFFT(hi_img, pred_img):
         fft_vals = fft_vol * tf.cast(k_vals, dtype=tf.complex64)
         
         # reg_val += tf.contrib.distributions.percentile(tf.math.abs(fft_vals), q=95, axis=None)
-        reg_val += tf.reduce_mean(tf.math.abs(fft_vals), axis=None)
+        reg_val += tf.reduce_sum(tf.reduce_mean(tf.math.abs(fft_vals), axis=[1, 2, 3, 4]), axis=None)
 
     return reg_val
 
@@ -73,7 +73,7 @@ def reg3DFFT(hi_img, pred_img):
         fft_vals = flat_fft[0:int(N/2)] * tf.cast(k_vals[0:int(N/2)], dtype=tf.complex64)
         
         # reg_val += tf.contrib.distributions.percentile(tf.math.abs(fft_vals), q=95, axis=None)
-        reg_val += tf.reduce_mean(tf.math.abs(fft_vals), axis=None)
+        reg_val += tf.reduce_sum(tf.reduce_mean(tf.math.abs(fft_vals), axis=[1, 2, 3, 4]), axis=None)
 
     return reg_val
 
